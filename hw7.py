8a31885c64a0a51fc8cc4b88fb569a6a376ab00f
Tennison <tenoverpar@gmail.com>
2017-02-23 19:45:53 -0800

    adding homework7 to my local repo

---
+++ hw7.py
@@ -0,0 +1,25 @@
+from collections import OrderedDict
+
+print( 'Regular dictionary')
+d = {}
+d['a'] = 'A'
+d['b'] = 'B'
+d['c'] = 'C'
+d['d'] = 'D'
+d['e'] = 'E'
+
+for k, v in d.items():
+    print (k, v)
+
+# This is the 2nd part
+print ('\nOrderedDict:')
+d = OrderedDict()
+d['a'] = 'A'
+d['b'] = 'B'
+d['c'] = 'C'
+d['d'] = 'D'
+d['e'] = 'E'
+
+for k, v in d.items():
+    print (k, v)
+    
